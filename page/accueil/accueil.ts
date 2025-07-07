12188bf4186c98d5527851d452851a1483a2732d
Pablo Garcia <pablogarousseau@gmail.com>
2025-07-07 14:52:18 +0200

    Etape 3

--- page/accueil/accueil.ts
+++ page/accueil/accueil.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../public/component/header/header'
-import { Section1 } from '../../public/component/section1/section1'
-import { Section2 } from '../../public/component/section2/section2'
+import { Section1 } from '../../page1E/section1/section1'
+import { Section2 } from '../../page1E/section2/section2'
 import { Footer } from '../../public/component/footer/footer'
-import { Section3 } from '../../public/component/section3/section3';
-import { Section4 } from '../../public/component/section4/section4';
+import { Section3 } from '../../page1E/section3/section3';
+import { Section4 } from '../../page1E/section4/section4';
 import { Contact } from '../../public/component/contact/contact';
 @Component({
   selector: 'app-accueil',
